d7aff36f4501d7ad5c6450b728c2938fa249e3e4
Furwarpur <furwarpur2828@gmail.com>
2026-05-08 22:15:43 +0900

    備品管理のフィルタの機能を使えるようにした

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:8080',
+        target: 'http://host.docker.internal:8081',
         changeOrigin: true,
       },
     },
